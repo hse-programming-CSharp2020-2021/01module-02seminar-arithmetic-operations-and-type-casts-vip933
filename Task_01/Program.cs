f5e1b0bcf054ae3e7db4e2e83b060abf322e8446
vip933 <71464354+vip933@users.noreply.github.com>
2020-11-15 20:20:36 +0300

    Update Program.cs

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Задача : 
  * Ввести значение x и вывести значение полинома: F(x) = 12x^4 + 9x^3 - 3x^2 + 2x – 4. 
  * С точностью до 2-х знаков после запятой. 
@@ -25,27 +25,46 @@
  */
 
 using System;
+using System.Globalization;
 
-namespace Task_01 {
-	class Program {
-		static void Main(string[] args) {
-			// TODO : Сменить локаль на "ru-RU". 
+namespace Task_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
+            double x;
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{Function(x):f2}");
+            Console.ReadLine();
 
-			double x;
-			// Ввод данных. 
-			x = double.Parse(Console.ReadLine());
+        }
 
-			// Вычисление и вывод данных.
-			Console.WriteLine($"{Function(x):f2}");
+        static double Function(double x)
+        {
+            double result = 12 * MyPow(x, 4) + 9 * MyPow(x, 3) - 3 * MyPow(x, 2) + 2 * x - 4;
+            return result;
+        }
 
-		}
-
-		static double Function(double x) {
-			// TODO : Реализовать вычисление функции F(x).
-		}
-
-		static double myPow(double x, int pow) {
-			// TODO : Реализовать быстрое возведение в степень.
-		}
-	}
+        static double MyPow(double x, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow % 2 == 1)
+                {
+                    return x * MyPow(x, pow - 1);
+                }
+                else
+                {
+                    x = MyPow(x, pow / 2);
+                    return x * x;
+                }
+            }
+        }
+    }
 }
